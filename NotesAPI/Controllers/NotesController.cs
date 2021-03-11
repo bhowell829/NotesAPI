b1bfb9012184b62963d3f38060c81eebe17482d0
Brittany Howell <bhowell829@gmail.com>
2021-03-11 07:54:27 -0500

    added notes api code with json file persistence

---
+++ NotesAPI/Controllers/NotesController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesAPI.Data;
+using NotesAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/Notes")]
+    public class NotesController : ControllerBase
+    {
+        private readonly DataContext dataContext;
+        public NotesController()
+        {
+            dataContext = DataContext.Context;
+        }
+
+        [HttpGet("/api/Notes")]
+        public List<Note> GetNotes()
+        {
+            return dataContext.Notes;
+        }
+
+        [HttpGet("/api/Notes/{id}")]
+        public Note GetNote(int id)
+        {
+            var noteData = dataContext.Notes.FirstOrDefault(c => c.Id == id);
+
+            return noteData;
+        }
+
+        [HttpPost("/api/Notes")]
+        public Note AddNote([FromBody] Note note)
+        {
+            dataContext.Notes.Add(note);
+            dataContext.SaveJsonData();
+
+            return note;
+        }
+
+        [HttpPut("/api/Notes/{id}")]
+        public void UpdateNote(int id, [FromBody] Note note)
+        {
+            for (var index = dataContext.Notes.Count - 1; index >= 0; index--)
+            {
+                if (dataContext.Notes[index].Id == id)
+                {
+                    dataContext.Notes[index] = note;
+                }
+            }
+            dataContext.SaveJsonData();
+        }
+
+        [HttpDelete("/api/Notes/{id}")]
+        public void DeleteNote(int id)
+        {
+            for (var index = dataContext.Notes.Count - 1; index >= 0; index--)
+            {
+                if (dataContext.Notes[index].Id == id)
+                {
+                    dataContext.Notes.RemoveAt(index);
+                }
+            }
+            dataContext.SaveJsonData();
+        }
+    }
+}
---
+++ NotesAPI/Data/DataContext.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using NotesAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesAPI.Data
+{
+    public class DataContext
+    {
+        private static readonly DataContext _dataContext;
+        public List<Note> Notes = new List<Note>();
+
+        static DataContext()
+        {
+            _dataContext = new DataContext();
+        }
+
+        private DataContext()
+        {
+            FillData();
+        }
+
+        public static DataContext Context
+        {
+            get
+            {
+                return _dataContext;
+            }
+        }
+
+        public void FillData()
+        {
+            string noteData = File.ReadAllText("notesdata.json");
+            List<Note> tempNotes = JsonConvert.DeserializeObject<List<Note>>(noteData);
+
+            if (tempNotes != null)
+            {
+                foreach (var note in tempNotes)
+                {
+                    Notes.Add(new Note { Id = note.Id, Title = note.Title, Body = note.Body, IsComplete = note.IsComplete });
+                }
+            }
+        }
+
+        public void SaveJsonData()
+        {
+            string newNoteData = JsonConvert.SerializeObject(Notes, Formatting.Indented);
+            File.WriteAllText("notesdata.json", newNoteData);
+        }
+    }
+}
